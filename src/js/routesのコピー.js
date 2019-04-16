b918a4cdf364af98163c162db8826f6cf027ad3a
33750900053607 <47789111+33750900053607@users.noreply.github.com>
2019-04-16 14:12:17 +0900

    Initial commit

---
+++ src/js/routesのコピー.js
@@ -0,0 +1,50 @@
+
+import HomePage from '../pages/home.f7.html';
+import AboutPage from '../pages/about.f7.html';
+import FormPage from '../pages/form.f7.html';
+import CatalogPage from '../pages/catalog.f7.html';
+import ProductPage from '../pages/product.f7.html';
+import SettingsPage from '../pages/settings.f7.html';
+
+// import DynamicRoutePage from '../pages/dynamic-route.f7.html';
+// import RequestAndLoad from '../pages/request-and-load.f7.html';
+import NotFoundPage from '../pages/404.f7.html';
+
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+  },
+  {
+    path: '/form/',
+    component: FormPage,
+  },
+  {
+    path: '/catalog/',
+    component: CatalogPage,
+  },
+  {
+    path: '/product/:id/',
+    component: ProductPage,
+  },
+  {
+    path: '/settings/',
+    component: SettingsPage,
+  },
+
+  // {
+  //   path: '/dynamic-route/blog/:blogId/post/:postId/',
+  //   component: DynamicRoutePage,
+  // },
+
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
+
+export default routes;
